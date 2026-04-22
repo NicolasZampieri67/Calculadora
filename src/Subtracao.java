748788df25418b0ce0b4f51026bd72387171f1c5
NicolasZampieri67 <nicolaszampieri43@gmail.com>
2026-04-22 14:13:30 -0300

    feat: adiciona função subtração

---
+++ src/Subtracao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacoes;
+
+
+
+public class Subtracao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        System.out.println(x + y);
+    }
+}
